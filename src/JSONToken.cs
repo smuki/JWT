7b2e8a6b4635f28e34f484c9a4fae1e6c7599198
sunrise <sunriseyuen@qq.com>
2016-11-28 19:35:39 +0800

    -

--- src/JSONToken.cs
+++ src/JSONToken.cs
@@ -22,36 +22,26 @@ namespace Volte.Data.Token
             };
         }
 
-        public static string Encode(JSONObject payload, string key, JwtHashAlgorithm algorithm)
+        public static string Encode(JSONObject payload , string key , JwtHashAlgorithm algorithm)
         {
-            return Encode(new JSONObject() , payload , Encoding.UTF8.GetBytes(key) , algorithm);
+            return Encode(payload , Encoding.UTF8.GetBytes(key) , algorithm);
         }
 
-        public static string Encode(JSONObject payload, byte[] key, JwtHashAlgorithm algorithm)
-        {
-            return Encode(new JSONObject(), payload, key, algorithm);
-        }
-
-        public static string Encode(JSONObject extraHeaders , JSONObject payload , string key , JwtHashAlgorithm algorithm)
-        {
-            return Encode(extraHeaders, payload, Encoding.UTF8.GetBytes(key), algorithm);
-        }
-
-        public static string Encode(JSONObject extraHeaders, object payload, byte[] key, JwtHashAlgorithm algorithm)
+        public static string Encode(JSONObject payload , byte[] key , JwtHashAlgorithm algorithm)
         {
             var segments = new List<string>();
-            JSONObject header = extraHeaders;
+            JSONObject header = new JSONObject();
             header.SetValue("typ" , "JWT");
             header.SetValue("alg" , algorithm.ToString());
 
-            var headerBytes = Encoding.UTF8.GetBytes(header.ToString());
+            var headerBytes  = Encoding.UTF8.GetBytes(header.ToString());
             var payloadBytes = Encoding.UTF8.GetBytes(payload.ToString());
 
             segments.Add(Base64UrlEncode(headerBytes));
             segments.Add(Base64UrlEncode(payloadBytes));
 
             var stringToSign = string.Join(".", segments.ToArray());
-            var bytesToSign = Encoding.UTF8.GetBytes(stringToSign);
+            var bytesToSign  = Encoding.UTF8.GetBytes(stringToSign);
 
             var signature = HashAlgorithms[algorithm](key, bytesToSign);
             segments.Add(Base64UrlEncode(signature));
@@ -59,34 +49,29 @@ namespace Volte.Data.Token
             return string.Join(".", segments.ToArray());
         }
 
-        public static string Decode(string token, string key, bool verify = true)
+        public static string Decode(string token , string key , bool verify = true)
         {
             return Decode(token, Encoding.UTF8.GetBytes(key), verify);
         }
 
-        public static string Decode(string token, byte[] key, bool verify = true)
+        public static string Decode(string token , byte[] key , bool verify = true)
         {
             var parts = token.Split('.');
-            if (parts.Length != 3)
-            {
-                throw new ArgumentException("Token must consist from 3 delimited by dot parts");
+            if (parts.Length != 3) {
+                payloadJson = "";
+                //throw new ArgumentException("Token must consist from 3 delimited by dot parts");
             }
 
             var payload = parts[1];
             var payloadJson = Encoding.UTF8.GetString(Base64UrlDecode(payload));
 
-            if (verify)
-            {
+            if (verify) {
                 Verify(payload, payloadJson, parts, key);
+            }else{
+                payloadJson = "";
             }
 
             return payloadJson;
-        }
-
-        public static JSONObject DecodeToObject(string token , byte[] key , bool verify = true)
-        {
-            var payloadJson = Decode(token, key, verify);
-            return new JSONObject(payloadJson);
         }
 
         public static string Base64UrlEncode(byte[] input)
@@ -114,39 +99,41 @@ namespace Volte.Data.Token
             return converted;
         }
 
-        public static void Verify(string payloadJson, string decodedCrypto, string decodedSignature)
+        public static bool Verify(string payloadJson, string decodedCrypto, string decodedSignature)
         {
-            if (decodedCrypto != decodedSignature)
-            {
-                throw new SignatureVerificationException(string.Format("Invalid signature. Expected {0} got {1}", decodedCrypto, decodedSignature));
+            if (decodedCrypto != decodedSignature) {
+                return false;
+                //throw new SignatureVerificationException(string.Format("Invalid signature. Expected {0} got {1}", decodedCrypto, decodedSignature));
             }
 
             // verify exp claim https://tools.ietf.org/html/draft-ietf-oauth-json-web-token-32#section-4.1.4
             JSONObject payloadData = new JSONObject(payloadJson);
 
-            int expInt=payloadData.GetInteger("exp");
+            int expInt = payloadData.GetInteger("exp");
 
             var secondsSinceEpoch = Math.Round((DateTime.UtcNow - UnixEpoch).TotalSeconds);
             if (secondsSinceEpoch >= expInt) {
-                throw new TokenExpiredException("Token has expired.");
+                return false;
+                //throw new TokenExpiredException("Token has expired.");
             }
+            return true;
         }
 
-        private static void Verify(string payload, string payloadJson, string[] parts, byte[] key)
+        private static bool Verify(string payload, string payloadJson, string[] parts, byte[] key)
         {
-            var crypto = Base64UrlDecode(parts[2]);
+            var crypto        = Base64UrlDecode(parts[2]);
             var decodedCrypto = Convert.ToBase64String(crypto);
 
-            var header = parts[0];
-            var headerJson = Encoding.UTF8.GetString(Base64UrlDecode(header));
+            var header            = parts[0];
+            var headerJson        = Encoding.UTF8.GetString(Base64UrlDecode(header));
             JSONObject headerData = new JSONObject(headerJson);
-            var algorithm = headerData.GetValue("alg");
+            var algorithm         = headerData.GetValue("alg");
 
-            var bytesToSign = Encoding.UTF8.GetBytes(string.Concat(header, ".", payload));
-            var signatureData = HashAlgorithms[GetHashAlgorithm(algorithm)](key, bytesToSign);
+            var bytesToSign      = Encoding.UTF8.GetBytes(string.Concat(header, ".", payload));
+            var signatureData    = HashAlgorithms[GetHashAlgorithm(algorithm)](key, bytesToSign);
             var decodedSignature = Convert.ToBase64String(signatureData);
 
-            Verify(payloadJson, decodedCrypto, decodedSignature);
+           return Verify(payloadJson, decodedCrypto, decodedSignature);
         }
 
         private static JwtHashAlgorithm GetHashAlgorithm(string algorithm)
